401fb00d5030ffc22a43fc6a42144770d4796997
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-10-19 04:48:59 +0530

    Updates

---
+++ precache-manifest.8eae343193016ffc7d7a77c083e96ad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b60bf26522b52e75f85ca34035f6de",
+    "url": "/sl-license-plate-validator/index.html"
+  },
+  {
+    "revision": "ed748284313dc911ae5e",
+    "url": "/sl-license-plate-validator/static/css/main.3aae7f0c.chunk.css"
+  },
+  {
+    "revision": "79a76394ac4eb256c337",
+    "url": "/sl-license-plate-validator/static/js/2.9555e78b.chunk.js"
+  },
+  {
+    "revision": "ed748284313dc911ae5e",
+    "url": "/sl-license-plate-validator/static/js/main.6cea0022.chunk.js"
+  },
+  {
+    "revision": "d12e4c57d1d694e9b58a",
+    "url": "/sl-license-plate-validator/static/js/runtime-main.e937124c.js"
+  }
+]);
